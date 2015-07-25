b4d748bd42e6553106b504d5af39a3fa08ebcf2c
Eric Wooley <ericwooley@gmail.com>
2015-07-25 09:49:10 -0700

    pushing the things

---
+++ client/ts/react-test.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../typings/tsd.d.ts"/>
+import React  = require('react');
+export default class Rtest extends React.Component<any, any> {
+  constructor() {
+    super();
+    console.log('works');
+  }
+}
+console.log('happens')
